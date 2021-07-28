1ca89a024e72cac5540d1ab0e0855dce5b524f85
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-28 09:21:10 +0200

    initial commit

---
+++ src/components/Alert.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="!flag" style="color: red">Name changed!</div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  props: ['user'],
+  setup(props) {
+    // context.emit('eventName' parameters);
+    const flag = computed(() => {
+      return props.user.name === 'John';
+    });
+
+    return {
+      flag,
+    };
+  },
+};
+</script>
